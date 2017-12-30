cc9f11dfbc0475e623654870df0fe29c8c65ccc5
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-12-30 05:00:38 +0000

    Pir Sensor - DONE

--- iot_helper/childPresenca.js
+++ iot_helper/childPresenca.js
@@ -1,18 +1,31 @@
+var sleep = require('sleep');
 var Gpio = require('onoff').Gpio,
   led = new Gpio(27, 'out'),
   pir = new Gpio(17, 'in', 'both');
 
-pir.watch(function(err, value) {
-  if (err) exit();
+setInterval(function(){ 
+	var value = pir.readSync();
+			//console.log(value);
+			
+			led.writeSync(value);
+			if(value == 1) sleep.sleep(3);
+		},1000);
 
+/*
+pir.watch(function(err, value) {
+  if (err) {
+	  console.log(err);
+	  exit();
+  }
   //1 para ligado e 0 para desligado
-  led.writeSync(value);
+  console.log(value);
+ 
+	led.writeSync(value);
+	if(value == 1) sleep.sleep(3);
 });
 
 function exit() {
     pir.unexport();
     led.unexport();
     process.exit();
-}
-
-//console.log("Ola Mundo");
+}*/
