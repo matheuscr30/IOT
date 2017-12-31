26b20c26d57d16e34eadb758a3271d7434f19f08
matheuscr30 <matheuscunhareis30@gmail.com>
2017-12-31 01:00:28 -0200

    Increase Lamp Control

--- controllers/lampadas.js
+++ controllers/lampadas.js
@@ -1,9 +1,9 @@
 module.exports.estadoAtual = function (application, req, res) {
     console.log(req.params.id);
     var Gpio = require('onoff').Gpio,
-      led = new Gpio(27, 'in', 'both');
+      lamp = new Gpio(27, 'in', 'both');
 
-    var value = led.readSync();
-    led.unexport();
+    var value = lamp.readSync();
+    lamp.unexport();
     res.json({"response" : "200", "estado" : value});
 }
--- iot_helper/childPresenca.js
+++ iot_helper/childPresenca.js
@@ -1,13 +1,13 @@
 var sleep = require('sleep');
 var Gpio = require('onoff').Gpio,
-  led = new Gpio(27, 'out'),
+  lamp = new Gpio(27, 'out'),
   pir = new Gpio(17, 'in', 'both');
 
-setInterval(function(){ 
+setInterval(function(){
 	var value = pir.readSync();
 			//console.log(value);
-			
-			led.writeSync(value);
+
+			lamp.writeSync(value);
 			if(value == 1) sleep.sleep(3);
 		},1000);
 
@@ -19,7 +19,7 @@ pir.watch(function(err, value) {
   }
   //1 para ligado e 0 para desligado
   console.log(value);
- 
+
 	led.writeSync(value);
 	if(value == 1) sleep.sleep(3);
 });
